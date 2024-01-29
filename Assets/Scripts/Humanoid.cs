da714ecc2703d47acb3adc8bab25696aa7e80ca8
Cliff Jean <47513811+cliff-lilly-jean@users.noreply.github.com>
2024-01-29 09:39:54 -0500

    initial commit and humanoid base class creation

---
+++ Assets/Scripts/Humanoid.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Humanoid : MonoBehaviour
+{
+    [Header("Descriptors")]
+    private string _name;
+    private int _age;
+
+    [Header("Attributes")]
+    private float _height;
+    private float _weight;
+    private float _stamina;
+
+}
